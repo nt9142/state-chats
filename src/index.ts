ba19604f3b3c9345c49713ccfcb83ee604b7174e
nik <nt9142@gmail.com>
2024-02-18 22:54:11 +0100

    fix: imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { getChat } from './core';
 export { useStateChat } from './react';
+export { processContent, getHasAction, executeAction } from './core/utils';
 
 export type * from './types';
